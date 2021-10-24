2024a1db574ccac017aba4e4a7bb3af226dd6d92
rasul-surname <rasul.surname@mail.ru>
2021-10-24 12:56:14 +0300

    message

---
+++ src/components/MyInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from "@mui/material/TextField";
+
+function MyInput({...props}) {
+
+
+    return (
+        <TextField {...props} className='form__input' id="outlined-basic" variant="outlined" size="small"/>
+    )
+}
+
+export default MyInput;
---
+++ src/redux/redux-store.js
@@ -0,0 +1,11 @@
+import {combineReducers, createStore} from "redux";
+import cardsReducer from "./cards-reducer";
+
+
+let reducers = combineReducers({
+    cardsPage: cardsReducer,
+})
+
+let store = createStore(reducers);
+
+export default store;
